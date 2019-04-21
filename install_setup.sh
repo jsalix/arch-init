4e728b1a7358f738084da763c6a88d378d632fe4
Josiah Salcido <commander225@gmail.com>
2019-04-20 20:29:40 -0700

    fixes

--- install_setup.sh
+++ install_setup.sh
@@ -10,9 +10,9 @@
 username="jsalcido"
 useradd -m -G wheel -s /bin/zsh "$username"
 
-su "$username" -c ./install_yay.sh
-su "$username" -c ./install_packages.sh
-su "$username" -c ./install_dotfiles.sh
+su - "$username" -c ./install_yay.sh
+su - "$username" -c ./install_packages.sh
+su - "$username" -c ./install_dotfiles.sh
 
 #read -s -p "Password:" password
 #echo "$password" | chpasswd "$username" --stdin
